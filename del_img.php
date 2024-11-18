74b693f369dc940f05d036e6514fb1c483aaa1c0
maplesift <a23050716@gmail.com>
2024-11-18 14:43:49 +0800

    增加刪除功能 並在名稱上加上時間戳

--- del_img.php
+++ del_img.php
@@ -1,9 +1,12 @@
 <?php
-
-$imgName=$_GET['file'];
+include_once "function.php";
+$id=$_GET['id'];
+$row=find('imgs',$id);
+$imgName=$row['filename'];
+// $imgName=find('imgs',$id)['filename'];
 unlink("./files/$imgName");
+del("imgs",$id);
 
 header("location:manage.php");
-
 
 ?>
